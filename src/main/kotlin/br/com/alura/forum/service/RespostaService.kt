13518400be0ec9120a42f6e5990fb31175bda568
Victor Vieira <contatovictorvieira@outlook.com>
2023-04-23 20:17:06 -0300

    cria conversão da entidade resposta para dto de entrada e de saída.

---
+++ src/main/kotlin/br/com/alura/forum/service/RespostaService.kt
@@ -0,0 +1,10 @@
+package br.com.alura.forum.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class RespostaService {
+    fun buscarPorId(id: Long){
+        return null
+    }
+}
